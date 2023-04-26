9ba7a618de00f9e26e3e5843fbb0a32a93cc66c3
rpjtesting <rpjtesting@gmail.com>
2023-04-27 02:21:09 +0530

    Able to render pdf in mobile finally

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
   webpack: (config) => {
     config.module.rules.push(
       {
--- src/components/ViewPdf.jsx
+++ src/components/ViewPdf.jsx
@@ -1,8 +1,12 @@
 import dynamic from "next/dynamic";
+import Image from "next/image";
 import { Document, Page } from "react-pdf";
 // import * as PDFJS from "pdfjs-dist/build/pdf";
 // import { PDFReader } from "react-read-pdf";
 // PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.min.js`;
+// import { Viewer, Worker } from "@react-pdf-viewer/core";
+// Import styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 const DynamicReactPdfViewer = dynamic(
   () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
@@ -18,9 +22,19 @@ const DynamicBlobProvider = dynamic(
   }
 );
 
-const DynamicPDFViewer = dynamic(() => import("./PDFViewer"), {
-  ssr: false,
-});
+const DynamicPDFWorker = dynamic(
+  () => import("@react-pdf-viewer/core").then((mod) => mod.Worker),
+  {
+    ssr: false,
+  }
+);
+
+const DynamicPDFViewer = dynamic(
+  () => import("@react-pdf-viewer/core").then((mod) => mod.Viewer),
+  {
+    ssr: false,
+  }
+);
 
 {
   /* <DynamicReactPdfViewer
@@ -36,7 +50,13 @@ const ViewPdf = ({ doc: MyPdfDocument, pdfData, isMobileNav }) => {
   return isMobileNav ? (
     <DynamicBlobProvider document={<MyPdfDocument data={pdfData} />}>
       {({ blob, url, loading }) => {
-        return loading ? "Loading Pdf..." : <DynamicPDFViewer url={url} />;
+        return loading ? (
+          "Loading Pdf..."
+        ) : (
+          <DynamicPDFWorker workerUrl="https://unpkg.com/pdfjs-dist@3.5.141/build/pdf.worker.min.js">
+            <DynamicPDFViewer fileUrl={url} />;
+          </DynamicPDFWorker>
+        );
       }}
     </DynamicBlobProvider>
   ) : (
