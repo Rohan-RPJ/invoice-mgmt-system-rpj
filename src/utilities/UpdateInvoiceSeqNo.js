2fe0b48443345f9961013edd18687218d83cbdde
rpjtesting <rpjtesting@gmail.com>
2023-09-12 03:33:42 +0530

    testing

--- src/utilities/UpdateInvoiceSeqNo.js
+++ src/utilities/UpdateInvoiceSeqNo.js
@@ -1,6 +1,6 @@
 var PropertiesReader = require("properties-reader");
 
-var filePath = "public/props/invoiceno.properties";
+var filePath = "./props/invoiceno.properties";
 var properties = PropertiesReader(filePath, { writer: { saveSections: true } });
 
 class UpdateInvoiceSeqNo {
