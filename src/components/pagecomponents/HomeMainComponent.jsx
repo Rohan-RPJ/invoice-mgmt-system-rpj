e0b23b762ea7f1837db4864f19a026eecbbbfc02
rpjtesting <rpjtesting@gmail.com>
2024-01-21 05:01:27 +0530

    minor change

--- src/components/pagecomponents/HomeMainComponent.jsx
+++ src/components/pagecomponents/HomeMainComponent.jsx
@@ -3,8 +3,13 @@ import React from "react";
 
 const HomeMainComponent = (isMobileNav) => {
   return (
-    <div className="w-full h-full p-52">
-      <Link href={"http://localhost:3000/invoice/create"}>Create Invoice</Link>
+    <div className="w-full h-full p-44">
+      {/* <Link href={"http://localhost:3000/invoice/create"}>Create Invoice</Link> */}
+      <h1 className={`text-xl p-5 font-bold`}>
+        Hi, Please Click on Create Invoice Link above to start creating your
+        invoice!
+      </h1>
+      <h2 className={`text-lg p-5 font-semibold italic`}>Your feedback really matters</h2>
     </div>
   );
 };
