c07ba191efea784b884fe355fa5ccb3019163bb9
rpjtesting <rpjtesting@gmail.com>
2024-01-21 13:47:10 +0530

    formatted

--- src/components/pagecomponents/HomeMainComponent.jsx
+++ src/components/pagecomponents/HomeMainComponent.jsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import {
-  FaceSmileIcon,
-} from "@heroicons/react/24/solid";
+import { FaceSmileIcon } from "@heroicons/react/24/solid";
 const HomeMainComponent = (isMobileNav) => {
   return (
     <div className="w-full h-full md:p-44 sm:p-4">
@@ -11,7 +9,8 @@ const HomeMainComponent = (isMobileNav) => {
         Hi, Please Click on Create Invoice Link to start creating your invoice!
       </h1>
       <h2 className={`text-lg p-5 font-semibold italic`}>
-        Your feedback really matters  <FaceSmileIcon className={`h-6 w-6 text-green-500`} />
+        Your feedback really matters{" "}
+        <FaceSmileIcon className={`h-6 w-6 text-green-500`} />
       </h2>
     </div>
   );
