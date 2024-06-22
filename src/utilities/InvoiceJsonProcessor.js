b1a534e82f2ca76e9f238973ad1fc92a7d648645
Rohan-RPJ <rohanpjain879@gmail.com>
2024-06-23 04:26:58 +0530

    unit price updated for withtax

--- src/utilities/InvoiceJsonProcessor.js
+++ src/utilities/InvoiceJsonProcessor.js
@@ -71,6 +71,7 @@ class InvoiceJsonProcessor {
         prodTotAmt = item.rate * item.qty;
         prodGstAmt = prodTotAmt * (item.gstPercent / (100 + item.gstPercent));
         prodNetAmt = prodTotAmt - prodGstAmt;
+        item.rate = prodNetAmt / item.rate
       } else {
         prodNetAmt = item.rate * item.qty;
         prodGstAmt = (prodNetAmt * item.gstPercent) / 100;
